dd4531a41a01b49f810cacb7c831701bfb9d4206
Nik Erzetic <nikerzetic@gmail.com>
2020-09-29 16:12:09 +0200

    Porocilo

--- ukrivljenosti.m
+++ ukrivljenosti.m
@@ -2,7 +2,10 @@ function u = ukrivljenosti(s,b,d)
 % UKRIVLJENOSTI    Izracuna ukrivljenosti Bezierjeve krivulje.
 %   UKRIVLJENOSTI(S) izracuna vektor ukrivljenosti u Bezierjeve krivulje b
 %   pri naravni parametrizaciji S. Parameter d je razdalja med sosednjima 
-%   tockama. Drugi odvod aproksimira s sosednjima tockama na sledec naci:
+%   tockama. Drugi in drugi odvod aproksimira s sosednjima tockama na 
+%   sledec naci:
+% 
+%       r'(s(i)) = (r(s(i)+h) - r(s(i)-h)) / 2*h
 % 
 %       r''(s(i)) = (r(s(i)+h) - 2r(s(i)) + r(s(i)-h)) / h^2
 % 
@@ -14,7 +17,9 @@ u = zeros(m,1);
 h = d/1000;
 
 for i = 2:m-1
-     u(i) = norm(deCasteljau(b,s(i)+h) - 2*deCasteljau(b,s(i)) + deCasteljau(b,s(i)-h))/h/h;
+    dbi = [(deCasteljau(b,s(i)+h) - deCasteljau(b,s(i)-h))/2/h;0];
+    ddbi = [(deCasteljau(b,s(i)+h) - 2*deCasteljau(b,s(i)) + deCasteljau(b,s(i)-h))/h/h;0];
+    u(i) = norm(cross(dbi,ddbi),1)/(norm(dbi)^3);
 end
 
 u(1) = u(2);
